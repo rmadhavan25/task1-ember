8e96e7386cf3812912bfeadd415cdf010374b292
rmadhavan25 <rmadhavan718@gmail.com>
2021-05-17 20:46:01 +0530

    proper table component, keyword highlight ,file/folder category

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -2,24 +2,29 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action} from '@ember/object';
 
-let result = "";
+
 export default class SearchController extends Controller {
 
     //variable to store the results from api call
     @tracked result;
+    @tracked loading;
     
     //function to fetch the files using the user inputs
     @action
     async getFiles(directoryPath,keyword){
         console.log(directoryPath,keyword)
-        if(directoryPath==undefined || keyword==undefined || keyword=="" || directoryPath==""){
-            directoryPath="*";
-            keyword="*";
+        if(directoryPath==undefined || keyword==undefined || directoryPath=="" || keyword==""){
+            alert('Please enter all the details');
         }
-        let response = await fetch(`http://localhost:8080/getfile?keyword=${keyword}&directoryPath=${directoryPath}`);
-        let data = await response.json();
-        console.log(data);
-        this.result = data;
+        else{
+            this.loading = "loading...";
+            let response = await fetch(`http://localhost:9090/SampleWebApp/getfile?keyword=${keyword}&directoryPath=${directoryPath}`);
+            let data = await response.json();
+            console.log(data);
+            this.result = data;
+            this.loading="No Files To Display";
+        }
+        
         
     }
     
